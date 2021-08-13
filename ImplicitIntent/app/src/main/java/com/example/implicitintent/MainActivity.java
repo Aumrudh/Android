e40a4fb67cb75c26bf81e8a98ed997f51c66820b
Aumrudh <aumrudhofficial@gmail.com>
2021-08-13 19:33:33 +0530

    final

--- ImplicitIntent/app/src/main/java/com/example/implicitintent/MainActivity.java
+++ ImplicitIntent/app/src/main/java/com/example/implicitintent/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.implicitintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.invoke);
+        //editText =  findViewById(R.id.editText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //String url=editText.getText().toString();
+                //Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                Intent intent = new Intent();
+                intent.setAction(android.content.Intent.ACTION_VIEW);
+                intent.setType("image/*");
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                //startActivity(intent);
+            }
+        });
     }
 }
