2f7b0642d411de885ed798e442cdaa4993423a72
Aumrudh <aumrudhofficial@gmail.com>
2021-08-17 19:42:59 +0530

    final

--- PopUp/app/src/main/java/com/example/popup/MainActivity.java
+++ PopUp/app/src/main/java/com/example/popup/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.popup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText e = (EditText)findViewById(R.id.value);
+        Button b = (Button)findViewById(R.id.convert);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popup = new PopupMenu(MainActivity.this, b);
+                popup.getMenuInflater().inflate(R.menu.pop_menu, popup.getMenu());
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        String choice=item.getTitle().toString();
+                        if(choice.equals("Dollar to Rupees")){
+                            String res =String.valueOf(Float.parseFloat(String.valueOf(e.getText()))*72);
+                            Toast.makeText(getApplicationContext(),res,Toast.LENGTH_LONG).show();
+                        }
+                        else if(choice.equals("Rupees to Dollar")){
+                            String res =String.valueOf(Float.parseFloat(String.valueOf(e.getText()))/72);
+                            Toast.makeText(getApplicationContext(),res,Toast.LENGTH_LONG).show();
+                        }
+                        return true;
+                    }
+                });
+                popup.show();
+            }
+        });
     }
 }
